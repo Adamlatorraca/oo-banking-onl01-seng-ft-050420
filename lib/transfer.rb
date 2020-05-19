444c900fdbf324d56b450a845bf5f7e6f8c8ff11
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-19 15:21:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
 
   def execute_transaction
     if sender.balance > amount
-      receiver.deposit(amount) && sender.deposit(-amount)
+      receiver.deposit(amount) && sender.deposit(-amount) && transfer.status = "complete"
     else
       "Insufficient funds"
     end
