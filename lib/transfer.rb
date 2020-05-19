ee7f799fbf2aa2043f05c8505ec7ce26d7bb3114
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-19 15:35:09 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
     if @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
-      @status = reversed
+      @status = "reversed"
     else
       return "Can't be reversed."
     end
